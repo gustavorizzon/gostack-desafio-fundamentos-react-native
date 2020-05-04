8624c055d42b4577d860d4e7d79506ef86b13030
gustavorizzon <gustavop.rizzon@gmail.com>
2020-05-03 21:13:18 -0300

    done: challenge solution

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -30,23 +30,103 @@ const CartProvider: React.FC = ({ children }) => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const cart = await AsyncStorage.getItem('@GoMarketplace:cart');
+
+      if (cart) {
+        setProducts(JSON.parse(cart));
+      }
     }
 
     loadProducts();
   }, []);
 
-  const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
-  }, []);
+  useEffect(() => {
+    async function updateStorage(): Promise<void> {
+      await AsyncStorage.setItem(
+        '@GoMarketplace:cart',
+        JSON.stringify(products),
+      );
+    }
 
-  const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+    updateStorage();
+  }, [products]);
 
-  const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+  const increment = useCallback(
+    async id => {
+      const foundProductIndex = products.findIndex(p => p.id === id);
+      if (foundProductIndex < 0) {
+        return;
+      }
+
+      const { title, price, quantity, image_url } = products[foundProductIndex];
+
+      const updatedProduct = {
+        id,
+        title,
+        price,
+        image_url,
+        quantity: quantity + 1,
+      };
+
+      const cart = products.map(p => (p.id === id ? updatedProduct : p));
+
+      setProducts(cart);
+    },
+    [products],
+  );
+
+  const addToCart = useCallback(
+    async (product: Product) => {
+      const { id, title, image_url, price } = product;
+
+      const foundProductIndex = products.findIndex(p => p.id === id);
+      if (foundProductIndex >= 0) {
+        increment(id);
+        return;
+      }
+
+      const newProduct = {
+        id,
+        title,
+        image_url,
+        price,
+        quantity: 1,
+      };
+
+      const cart = [...products, newProduct];
+
+      setProducts(cart);
+    },
+    [products, increment],
+  );
+
+  const decrement = useCallback(
+    async id => {
+      const foundProductIndex = products.findIndex(p => p.id === id);
+      if (foundProductIndex < 0) {
+        return;
+      }
+
+      const { title, price, quantity, image_url } = products[foundProductIndex];
+
+      if (quantity <= 1) {
+        return;
+      }
+
+      const updatedProduct = {
+        id,
+        title,
+        price,
+        image_url,
+        quantity: quantity - 1,
+      };
+
+      const cart = products.map(p => (p.id === id ? updatedProduct : p));
+
+      setProducts(cart);
+    },
+    [products],
+  );
 
   const value = React.useMemo(
     () => ({ addToCart, increment, decrement, products }),
